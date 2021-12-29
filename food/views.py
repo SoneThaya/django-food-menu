187b69c333f7b84c5b485650105ea4659e9aa092
sone <sthaya@gmail.com>
2021-12-29 14:03:49 -0800

    created item form to edit and create item, setup delete route

--- food/views.py
+++ food/views.py
@@ -1,9 +1,11 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from food.forms import ItemForm
 from .models import Item
 from django.template import loader
 
-# Create your views here.
+
 def index(request):
     item_list = Item.objects.all()
     context = {
@@ -22,3 +24,34 @@ def detail(request, pk):
         'item': item,
     }
     return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-form.html', {'form':form})
+
+
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-form.html', {'form':form, 'item': item})
+
+
+def delete_item(request, id):
+    item = Item.objects.get(id=id)
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-delete.html', {'item':item})
